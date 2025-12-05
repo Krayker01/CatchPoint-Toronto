74aeceb331d98af5b84f4e8f9de3e730ffcd0a0d
Krayker01 <107270701+Krayker01@users.noreply.github.com>
2025-12-04 22:25:23 -0500

    feat: initialize pages and prepare layered images - Initialized basic project pages - Created images for the Intro - Split images into layers (background and notes) - Prepared structure for implementing parallax

--- frontend/CatchPoint/src/App.jsx
+++ frontend/CatchPoint/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import Intro from './pages/Intro'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
   )
 }
 
