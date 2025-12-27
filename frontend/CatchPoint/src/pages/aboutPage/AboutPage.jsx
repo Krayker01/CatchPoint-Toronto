1c401e63e3a95eab44f0ba51abad4fc80b619a23
Krayker01 <107270701+Krayker01@users.noreply.github.com>
2025-12-27 18:21:14 -0500

    feat: initial AboutPage created

--- frontend/CatchPoint/src/pages/aboutPage/AboutPage.jsx
+++ frontend/CatchPoint/src/pages/aboutPage/AboutPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Footer from '../Intro/Footer.jsx'
+import '../../../public/assets/background/background.css'
 
 const AboutPage = () => {
     return (
-        <div>AboutPage</div>
+        <div className="background-wrapper">
+            <div className="background">
+
+                <h1>AboutPage</h1>
+                <Footer />
+
+            </div>
+        </div>
     )
 }
 
